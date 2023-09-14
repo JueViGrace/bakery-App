6b8b211ff7e9dc558fe428d20daad10fff3bb2be
pablo castillo villalobos <hgmopikr@hotmail.com>
2023-09-14 16:24:59 -0400

    versiones

--- app/src/main/java/com/bakery/bakeryapp/constantes/Constantes.kt
+++ app/src/main/java/com/bakery/bakeryapp/constantes/Constantes.kt
@@ -8,7 +8,7 @@ import java.util.Locale
 object Constantes {
     const val version: String = BuildConfig.VERSION_NAME
     const val build: String = BuildConfig.BUILD_TYPE
-    const val BASE_URL = "https://7c50-45-186-203-254.ngrok-free.app/api/"
+    const val BASE_URL = "https://fce1-45-186-203-254.ngrok-free.app/api/"
     const val UPDATE_APP_REQUEST_CODE: Int = 200
     const val DATABASE_VERSION_OLD: Int = 0
     const val DATABASE_VERSION_NEW: Int = 2
@@ -19,10 +19,10 @@ object Constantes {
     // variables para dar formato a los numeros
     val formato = DecimalFormat("#,##0.00")
     val formato2 = DecimalFormat("#,##0")
-    val ACCESS_TOKEN = "ACCESS_TOKEN_KEY"
-    val COD_USUARIO = "COD_USUARIO_KEY"
-    val NICK_USUARIO = "NICK_USUARIO_KEY"
-    val NOMBRE_USUARIO = "NOMBRE_USUARIO_KEY"
-    val CLEAR_PREFERENCES = "CLEAR_PREFERENCES_KEY"
+    const val ACCESS_TOKEN = "ACCESS_TOKEN_KEY"
+    const val COD_USUARIO = "COD_USUARIO_KEY"
+    const val NICK_USUARIO = "NICK_USUARIO_KEY"
+    const val NOMBRE_USUARIO = "NOMBRE_USUARIO_KEY"
+    const val CLEAR_PREFERENCES = "CLEAR_PREFERENCES_KEY"
     // fun LocalDateTime.toMillis() = this.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
 }
