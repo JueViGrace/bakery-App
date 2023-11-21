ddfb196d0a71d6efb2d4f3c4ade07ccd6a5bdffa
pablo castillo villalobos <danielzabalanava@gmail.com>
2023-11-21 15:58:47 -0400

    versiones

--- app/src/main/java/com/bakery/bakeryapp/constantes/Constantes.kt
+++ app/src/main/java/com/bakery/bakeryapp/constantes/Constantes.kt
@@ -1,12 +1,11 @@
 package com.bakery.bakeryapp.constantes
 
 import com.google.android.play.core.ktx.BuildConfig
-import java.text.DecimalFormat
 
 object Constantes {
     const val version: String = BuildConfig.VERSION_NAME
     const val build: String = BuildConfig.BUILD_TYPE
-    const val BASE_URL = "https://6c4a-45-186-203-254.ngrok-free.app/api/"
+    const val BASE_URL = "https://b113-45-186-203-254.ngrok-free.app/api/"
     const val UPDATE_APP_REQUEST_CODE: Int = 200
     const val DATABASE_VERSION_OLD: Int = 0
     const val DATABASE_VERSION_NEW: Int = 3
@@ -14,9 +13,6 @@ object Constantes {
     const val DATASTORE_NAME = "PREFERECES"
     // val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT)
 
-    // variables para dar formato a los numeros
-    val formato = DecimalFormat("#,##0.00")
-    val formato2 = DecimalFormat("#,##0")
     const val ACCESS_TOKEN = "ACCESS_TOKEN_KEY"
     const val COD_USUARIO = "COD_USUARIO_KEY"
     const val NICK_USUARIO = "NICK_USUARIO_KEY"
